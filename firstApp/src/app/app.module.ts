efe51a072d233378fe35a9650839c40c050ddb03
Himanshu Kashyap <himanshu_kashyap@outlook.com>
2022-05-09 12:13:33 +0530

    sd

--- firstApp/src/app/app.module.ts
+++ firstApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewComponentComponent } from './new-component/new-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewComponentComponent
   ],
   imports: [
     BrowserModule,
