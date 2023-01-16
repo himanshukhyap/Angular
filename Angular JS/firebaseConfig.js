a762af384d15802a85960fed75102b9e7056f484
Himanshu Kashyap <90597724+cloud7info-himanshu@users.noreply.github.com>
2023-01-16 07:46:37 +0530

    ,m

---
+++ Angular JS/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBNJ2xZbUWM4dxREjufC2Lk6OYGvn322ww",
+  authDomain: "hostingdemo-30d7c.firebaseapp.com",
+  projectId: "hostingdemo-30d7c",
+  storageBucket: "hostingdemo-30d7c.appspot.com",
+  messagingSenderId: "125428751558",
+  appId: "1:125428751558:web:1018e0132aa8e58c7dcbc7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
